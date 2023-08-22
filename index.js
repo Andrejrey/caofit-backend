d3177d5f9f065896ac32926a2be5c76b2b912282
claudia-sikorski <c.sikorski@outlook.de>
2023-08-22 16:54:59 +0200

    add cors and GET request for /foodlist

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import app from "./app.js";
 import pool from "./db/client.js";
+import cors from "cors";
 
 const port = process.env.PORT || 8080;
+
+app.use(cors());
+
+app.route("/foodlist").get(async (req, res) => {
+  try {
+    const { rows } =
+      await pool.query(`SELECT FoodList.id as id, FoodList.name as name, image, icon, FoodCategory.name as category, quantity, FoodUnits.name as unit, carbs, fat, proteins, kcal
+    FROM FoodList
+    LEFT JOIN FoodCategory on FoodCategory.id = FoodList.category
+    LEFT JOIN FoodUnits on FoodUnits.id = FoodList.unit;`);
+    return res.json(rows);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
