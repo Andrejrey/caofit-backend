a057f811e5c50b342d2b3a40430d52271d411d52
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-08-24 21:57:54 +0200

    add post requests for Diary und FoodEaten table

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import app from "./app.js";
 import pool from "./db/client.js";
 import cors from "cors";
+import express from "express";
 
 const port = process.env.PORT || 8080;
 
 app.use(cors());
+app.use(express.json());
 
 app.route("/foodlist").get(async (req, res) => {
   try {
@@ -13,6 +15,71 @@ app.route("/foodlist").get(async (req, res) => {
     FROM FoodList
     LEFT JOIN FoodCategory on FoodCategory.id = FoodList.category
     LEFT JOIN FoodUnits on FoodUnits.id = FoodList.unit;`);
+    return res.json(rows);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.route("/total_nutritional_value").post(async (req, res) => {
+  try {
+    const { total_carbs, total_fats, total_proteins, total_kcal, date, day } =
+      req.body;
+    if (
+      !total_carbs ||
+      !total_fats ||
+      !total_proteins ||
+      !total_kcal ||
+      !date ||
+      !day
+    )
+      throw new Error("Invalid input", 400);
+    const { rows } = await pool.query(
+      "INSERT INTO Diary (total_carbs, total_fats, total_proteins, total_kcal, date, day) VALUES ($1, $2, $3, $4, $5, $6) RETURNING * ",
+      [total_carbs, total_fats, total_proteins, total_kcal, date, day]
+    );
+    return res.json(rows);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.route("/food_eaten").post(async (req, res) => {
+  try {
+    const {
+      food_icon,
+      food_name,
+      food_quantity,
+      food_unit,
+      food_total_carbs,
+      food_total_fats,
+      food_total_proteins,
+      food_total_kcal,
+    } = req.body;
+    if (
+      !food_icon ||
+      !food_name ||
+      !food_quantity ||
+      !food_unit ||
+      !food_total_carbs ||
+      !food_total_fats ||
+      !food_total_proteins ||
+      !food_total_kcal
+    )
+      throw new Error("Invalid input", 400);
+    const { rows } = await pool.query(
+      "INSERT INTO FoodEaten (food_icon, food_name, food_quantity, food_unit, food_total_carbs, food_total_fats, food_total_proteins, food_total_kcal) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING * ",
+      [
+        food_icon,
+        food_name,
+        food_quantity,
+        food_unit,
+        food_total_carbs,
+        food_total_fats,
+        food_total_proteins,
+        food_total_kcal,
+      ]
+    );
     return res.json(rows);
   } catch (error) {
     return res.status(500).json({ error: error.message });
